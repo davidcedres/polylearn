6506d10ac30593cc8b4efd03f3e7873a5f8b0abb
David Cedres <david.cedresx@gmail.com>
2023-10-14 21:31:00 -0400

    try something for uploading files

--- api/src/s3.ts
+++ api/src/s3.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { S3 } from "@aws-sdk/client-s3";
 
 export const s3 = new S3({
-    forcePathStyle: false,
     endpoint: "http://storage:9000",
+    forcePathStyle: true,
     region: "us-east-1",
     credentials: {
         accessKeyId: process.env.MINIO_ACCESS_KEY_ID!,
